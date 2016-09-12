e7ed248643b3e00029588e57915d2d96f0023fe4
linxubin <775426205@qq.com>
2016-09-12 11:12:26 +0800

    e

--- chrome-jelly/bg.js
+++
@@ -1,9 +0,0 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details)
-    {
-        console.log("url:"+ details.url);
-        console.log("requestBody:"+details.requestBody);
-    },
-    {urls: [ "https://*/*","http://*/*" ]},
-    ['requestBody']
-);
